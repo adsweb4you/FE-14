99395b8c16b905c3ce9b588b5f10a01a0fef830f
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2021-04-28 21:57:31 +0400

    საშინაო დავალება

--- js/dom.js
+++ js/dom.js
@@ -34,13 +34,7 @@ let myages = [15, 45, 30, 11150, 70, 90, 521];
 
 // console.log(myages.sort(sortable).reverse());
 
- let finded  =  myages.filter(forea);
-
-function forea(element){
-   return element < 100;
-}
-
- console.log(finded);
+ 
 
 // ------ მოქმედებები მასივზე   ------ //
 
@@ -60,3 +54,98 @@ function forea(element){
 
 //  აბრუნებს პირველივე  ელემენტებს პირობის მიხედვით
 //myages.find(forea);
+
+// ამოწმებს მასივს ერთ-ერთი ელემენტი თუ მაინც აკმაყოფილებს პირობას
+// myages.some(forea);
+
+//ამოწმებს მასივს ყველა ელემენტი თუ მაინც აკმაყოფილებს პირობას
+//myages.every(forea);
+
+let Students = [
+   {name:"ნინო", age:25, stipend:true, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"ნინო ნიკა 2", age:25, stipend:false, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"ვაკო", age:25, stipend:false, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"ზურა", age:25, stipend:false, regdate:"25/06/2011", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"შორენა", age:25, stipend:true, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"გიო", age:25, stipend:false, regdate:"25/06/2019", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"შალვა", age:25, stipend:true, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"ნინო", age:25, stipend:true, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"ნინო", age:25, stipend:true, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899},
+   {name:"ნინო", age:25, stipend:true, regdate:"25/06/2018", avatar:"https://cdn.icon-icons.com/icons2/1736/PNG/512/4043260-avatar-male-man-portrait_113269.png", phone:55847899}
+];
+
+
+ let stipstudent = Students.filter(checkstip);
+
+ function checkstip(arrr){
+    return arrr.stipend == true;
+ }
+
+ 
+ 
+
+
+let stud = document.getElementById("students");
+
+ 
+
+// სტიპენდიანტები
+let checks = document.getElementById("stipend");
+
+checks.addEventListener("change", function(e){
+
+   
+   stud.innerHTML = '';
+
+   if(!this.checked){
+         Students.forEach(outstudents)
+   }else{
+         stipstudent.forEach(outstudents)
+   }
+})
+// 
+
+let searc = document.getElementById("search");
+
+searc.addEventListener("input", function(e){
+
+ let vals = this.value;
+
+   let seearchedstu = Students.filter(sear);
+
+   function sear(arrr){
+      return !arrr.name.search(vals);
+   }
+ 
+   stud.innerHTML = '';
+
+ 
+   seearchedstu.forEach(outstudents)
+ 
+})
+// 
+
+Students.forEach(outstudents);
+
+function outstudents(studen){
+
+   if(studen.stipend){
+      var stip = '<span class="badge d-block  bg-success">სტიპენდიანტი</span>';
+   }else{
+      var stip = '<span class="badge d-block  bg-danger">ოროსანი</span>';
+   }
+
+   stud.innerHTML += `<div class="col-lg-4 mb-3"><div class="card">
+   <img src="${studen.avatar}" class="card-img-top" alt="...">
+   <div class="card-body">
+     <h5 class="card-title">${studen.name}</h5>
+     <p class="card-text">ტელ-ნომერი : ${studen.phone}</p>
+     <p class="btn btn-primary">ასაკი: ${studen.phone} </p>
+     ${stip}
+   </div>
+  </div>
+ </div>`
+}
+
+
+ 
